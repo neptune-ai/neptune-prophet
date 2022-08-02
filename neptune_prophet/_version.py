50860ffdbd878dc3104860b9835795a7a021ec28
Rafal Jankowski <rafal.jankowski@neptune.ai>
2022-08-02 14:29:18 +0200

    Update _version

--- neptune_prophet/_version.py
+++ neptune_prophet/_version.py
@@ -44,7 +44,7 @@ def get_config():
     cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = ""
-    cfg.versionfile_source = "kedro_neptune/_version.py"
+    cfg.versionfile_source = "neptune_prophet/_version.py"
     cfg.verbose = False
     return cfg
 
