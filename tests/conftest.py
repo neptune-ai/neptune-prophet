e21cde1829d8f2c55a76424d7805dd5f1777f28f
Rafał Jankowski <rafal.jankowski@neptune.ai>
2022-08-16 14:34:28 +0200

    Updated e2e structure (#7)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,29 @@
+# pylint: disable=redefined-outer-name
 import pandas as pd
 import pytest
-
 from prophet import Prophet
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def dataset():
-    return pd.read_csv("tests/dataset.csv")
+    yield pd.read_csv("tests/dataset.csv")
 
 
-@pytest.fixture()
-def model(dataset):  # pylint: disable=redefined-outer-name
-    model = Prophet()  # pylint: disable=redefined-outer-name
-    model.fit(dataset)
-    return model
+@pytest.fixture(scope="session")
+def model(dataset):
+    prophet_model = Prophet()
+    prophet_model.fit(dataset)
+    yield prophet_model
+
+
+@pytest.fixture(scope="session")
+def forecast(model):
+    future = model.make_future_dataframe(periods=365)
+    forecast = model.predict(future)
+    yield forecast
+
+
+@pytest.fixture(scope="session")
+def predicted(model, dataset):
+    predicted = model.predict(dataset)
+    yield predicted
