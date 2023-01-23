aa137e24b515ebc7640d00e39914879d7d05601d
Timothy Wolodzko <twolodzko@users.noreply.github.com>
2023-01-23 10:18:51 +0100

    Simplify unit tests & fix bug when forecast passed to create_summary was None (#25)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,29 @@
 import pandas as pd
 import pytest
+
 from prophet import Prophet
 
 
 @pytest.fixture(scope="session")
 def dataset():
-    yield pd.read_csv("tests/dataset.csv")
+    return pd.read_csv("tests/dataset.csv")
 
 
 @pytest.fixture(scope="session")
 def model(dataset):
     prophet_model = Prophet()
     prophet_model.fit(dataset)
-    yield prophet_model
+    return prophet_model
 
 
 @pytest.fixture(scope="session")
 def forecast(model):
     future = model.make_future_dataframe(periods=365)
     forecast = model.predict(future)
-    yield forecast
+    return forecast
 
 
 @pytest.fixture(scope="session")
 def predicted(model, dataset):
     predicted = model.predict(dataset)
-    yield predicted
+    return predicted
